4757471177b3b3052906bf41120f5b199e02740a
Zach Durham <ZachDurham@Zachs-MacBook-Pro.local>
2017-08-28 21:48:20 -0400

    adding customer model

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
   var Burger = sequelize.define("Burger", {
     burger_name: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 50]
+      }
     },
     devoured: {
       type: DataTypes.BOOLEAN,
